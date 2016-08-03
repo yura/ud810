c7d7a7881cd53093d8a47661fe320a5b91bc5fc1
Yury Kotlyarov <yura@brainhouse.ru>
2016-08-03 15:45:42 +0700

    Small improving in stubs' generation: main file template

--- problem_sets/create_dirs.rb
+++ problem_sets/create_dirs.rb
@@ -1,7 +1,9 @@
+#!/usr/bin/env ruby
+
 require 'fileutils'
 
 if ARGV.empty?
-  puts "Usage: ruby create_dirs.rb <#ps>"
+  puts "Usage: ruby create_dirs.rb <ps#>"
   exit 1
 end
 
@@ -10,24 +12,31 @@ ps_number = ARGV.first
 langs = { cpp: 'cpp', octave: 'm', python: 'py', ruby: 'rb' }
 
 langs.each do |lang, extension|
-  project_name = "ps#{ps_number}"
-  project_dir = "#{project_name}/#{project_name}_#{lang}"
-  FileUtils::mkdir_p "#{project_dir}/input"
-  FileUtils::mkdir_p "#{project_dir}/output"
+  problem_set = "ps#{ps_number}"
+  ps_dir = "#{problem_set}/#{problem_set}_#{lang}"
+  ps_file = "ps#{ps_number}.#{extension}"
+
+  FileUtils::mkdir_p "#{ps_dir}/input"
+  FileUtils::mkdir_p "#{ps_dir}/output"
 
   # copy templates
   if File.exist?("templates/#{lang}")
-    Dir["templates/#{lang}/*"].each do |file|
-      File.open("#{project_dir}/#{File.basename(file)}", 'w+') do |dest|
-        File.open(file) do |src|
-          dest.puts src.read.gsub('<project_name>', project_name)
+    Dir["templates/#{lang}/*"].each do |template|
+      destination_file_name = File.basename(template)
+
+      if destination_file_name == "main.#{extension}"
+        destination_file_name = ps_file
+      end
+
+      File.open(File.join(ps_dir, destination_file_name), 'w+') do |dest|
+        File.open(template) do |src|
+          dest.puts src.read.gsub('<ps#>', problem_set)
         end
       end
     end
   end
 
-  project_file = "#{project_dir}/ps#{ps_number}.#{extension}"
-  unless File.exist? project_file
-    FileUtils.touch project_file
+  unless File.exist? File.join(ps_dir, ps_file)
+    FileUtils.touch File.join(ps_dir, ps_file)
   end
 end
