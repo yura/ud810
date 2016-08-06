231c90ad55a6658894e9e562f0b2072ccf20a6b5
Yury Kotlyarov <yura@brainhouse.ru>
2016-08-07 00:08:13 +0700

    Adding template file for C++

---
+++ problem_sets/templates/cpp/main.cpp
@@ -0,0 +1,12 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv) {
+    Mat image;
+
+    return 0;
+}
